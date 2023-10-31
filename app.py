c32c6151435b7d6701e82f561a5373971e7c59e1
Noah-Gallego <82192653+Noah-Gallego@users.noreply.github.com>
2023-10-31 00:06:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def predict():
     return render_template('index.html', prediction_text='Predicted Iris Species: {}'.format(predicted_name))
 
 if __name__ == "__main__":
-    app.run(port = 5000, debug=True)
+    app.run(host='0.0.0.0', port=os.environ.get('PORT'))
+
